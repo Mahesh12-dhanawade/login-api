1fae2a70af0b077dbba32041ac03c42c122784f4
maheshvdhanawade <maheshgitt9324@gmail.com>
2022-11-21 22:07:42 +0530

    main commit

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
+// const express = require("express");
+// const app = express();
+// let mongoose = require("mongoose");
+// const port = process.env.PORT || 5000;
+// const cors = require("cors");
+// require('dotenv/config');
+// app.use(cors());
+
+// const AuthContoller = require("./controller/authContoller");
+// app.use("/api/auth", AuthContoller);
+
+// // app.listen(port,() => {
+// //     console.log(`listening on port ${port}`)
+// // })
+
+// let url = process.env.mongoUrl
+// mongoose
+//   .connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: "juneintern",
+//   })
+//   .then(() => {
+//     console.log("Database Connection is ready...");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// //Server
+// app.listen(port, () => {
+//   console.log(`server is running http://localhost:${port}`);
+// });
+
 const express = require("express");
 const app = express();
-require('dotenv').config()
-let mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv/config");
+let port = process.env.port || 5000
 app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
 const AuthContoller = require("./controller/authContoller");
 app.use("/api/auth", AuthContoller);
 
-// app.listen(port,() => {
-//     console.log(`listening on port ${port}`)
-// })
-
-let url = process.env.mongoUrl
-console.log(url)
+//Database
 mongoose
-  .connect(url, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: "juneintern",
@@ -30,7 +66,5 @@ mongoose
 
 //Server
 app.listen(port, () => {
-  console.log(`server is running http://localhost:${port}`);
+  console.log(`server is running on ${port}`);
 });
-
-
