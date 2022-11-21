d44b921f0c321022fe437e592e855329a36abb95
maheshvdhanawade <maheshgitt9324@gmail.com>
2022-11-21 19:07:33 +0530

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./db');
+require('dotenv').config()
+let mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-const AuthContoller = require('./controller/authContoller');
-app.use('/api/auth',AuthContoller);
+const AuthContoller = require("./controller/authContoller");
+app.use("/api/auth", AuthContoller);
 
-app.listen(port,() => {
-    console.log(`listening on port ${port}`)
-})
+// app.listen(port,() => {
+//     console.log(`listening on port ${port}`)
+// })
+let mongourl = process.env.mongoUrl
+mongoose
+  .connect(mongourl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "juneintern",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Server
+app.listen(port, () => {
+  console.log(`server is running http://localhost:${port}`);
+});
